e215b7e0eea1d56138b26fdc2570066b20af1994
isabelmelgui <143104109+isabelmelgui@users.noreply.github.com>
2023-10-02 11:57:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import glob
 from gtts import gTTS
 from PIL import Image
 #imagen
-import pytesseract
+#import pytesseract
 
 
 model = load_model('keras_model.h5')
@@ -108,26 +108,5 @@ def remove_files(n):
 remove_files(7)
 
 #Imagen
-st.title("Leer etiqueta")
 
-img_file_buffer = st.camera_input("Toma una Foto")
-
-with st.sidebar:
-      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
-
-
-if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-    
-    if filtro == 'Con Filtro':
-         cv2_img=cv2.bitwise_not(cv2_img)
-    else:
-         cv2_img= cv2_img
-    
-        
-    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text=pytesseract.image_to_string(img_rgb)
-    st.write(text) 
 
