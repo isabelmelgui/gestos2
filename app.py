faf958b5bfe64e3d36a0f34a6649f094d606ee92
isabelmelgui <143104109+isabelmelgui@users.noreply.github.com>
2023-10-05 19:25:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import glob
 from gtts import gTTS
 from PIL import Image
 #imagen
-#import pytesseract
+import pytesseract
 
 
 model = load_model('keras_model.h5')
@@ -106,3 +106,29 @@ def remove_files(n):
 
 
 remove_files(7)
+
+
+#Leer imagen
+
+st.title("Reconocimiento óptico de Caracteres")
+
+img_file_buffer2 = st.file_uploader("Choose a jpg file")
+
+with st.sidebar:
+      filtro2 = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
+
+
+if img_file_buffer2 is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer2.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    
+    if filtro2 == 'Con Filtro':
+         cv2_img =cv2.bitwise_not(cv2_img)
+    else:
+         cv2_img = cv_img
+    
+        
+    img_rgb2 = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+    text=pytesseract.image_to_string(img_rgb2)
+    st.write(text) 
