3ff9bb689649e9feddd87ec96c03c5a32bd34b6a
isabelmelgui <143104109+isabelmelgui@users.noreply.github.com>
2023-10-06 18:43:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 st.title("Lector de etiquetas")
-st.title('_Moda_ :pink[Inclusiva] :sunglasses:')
+st.title('_Moda_ :pink[Inclusiva]')
 
 img_file_buffer = st.camera_input("Toma una Foto")
 
